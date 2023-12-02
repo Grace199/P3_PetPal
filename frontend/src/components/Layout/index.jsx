b7e41dc3b6d4ec38697981b91978f2d3a35219aa
Dani3lx <1danielxu@gmail.com>
2023-12-02 18:00:50 -0500

    Added footer

--- frontend/src/components/Layout/index.jsx
+++ frontend/src/components/Layout/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Outlet } from "react-router-dom"
+import Footer from '../Footer'
 
 const Layout = () => {
     return (
         <>
             <header>header</header>
             <Outlet />
-            <footer>Footer</footer>
+            <Footer />
         </>
     )
 }
