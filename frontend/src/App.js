1ab53922e286d07aea466a27a20ffc2af6fae57a
Dani3lx <1danielxu@gmail.com>
2023-12-05 03:45:30 -0500

    Implemented navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,22 +7,24 @@ import PetListings from './pages/PetListings'
 import MyListings from './pages/MyListings'
 import Login from './pages/Login';
 import './App.css';
-
+import { UserContext, useUserContext } from './contexts/UserContext';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="petdetail/:petID" element={<PetDetail />} />
-          <Route path="petlistings/" element={<PetListings />} />
-          <Route path="mylistings/" element={<MyListings />} />
-          <Route path="login/" element={<Login />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <UserContext.Provider value={useUserContext()}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="petdetail/:petID" element={<PetDetail />} />
+            <Route path="petlistings/" element={<PetListings />} />
+            <Route path="mylistings/" element={<MyListings />} />
+            <Route path="login/" element={<Login />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -3,10 +3,48 @@ import { Link } from 'react-router-dom'
 import logo from '../../assets/images/logo.svg'
 import hamburger from '../../assets/images/hamburger.svg'
 import cross from '../../assets/images/cross.svg'
-import { useState } from 'react'
+import { useState, useEffect, useContext } from 'react'
+import { UserContext } from '../../contexts/UserContext';
+import { ajax_or_login } from '../../util/ajax';
 
 const Index = () => {
     const [openNav, setOpenNav] = useState(false);
+    const { id, setId } = useContext(UserContext);
+    const [avatar, setAvatar] = useState(
+        localStorage.getItem('avatar') || null,
+    )
+    const [isSeeker, setIsSeeker] = useState(false);
+    const [openProfileMenu, setOpenProfileMenu] = useState(false);
+
+    useEffect(() => {
+        async function fetchData() {
+            if (id !== -1) {
+                const isSeeker = JSON.parse(localStorage.getItem("isSeeker"));
+                const apiUrl = `/accounts/${isSeeker ? 'seeker' : 'shelter'}/${id}/`;
+                const res = await ajax_or_login(apiUrl, {
+                    method: "GET",
+                });
+
+                if (res.ok) {
+                    const data = await res.json();
+                    setAvatar(data.account.avatar);
+                    localStorage.setItem('avatar', data.account.avatar);
+                    setIsSeeker(isSeeker);
+                }
+            }
+        }
+
+        fetchData();
+    }, [id]);
+
+    function handleLogout() {
+        localStorage.removeItem('avatar');
+        localStorage.removeItem('access');
+        localStorage.removeItem('isSeeker');
+        setId(-1);
+        setIsSeeker(null);
+        setAvatar(null);
+    }
 
     return (
         <header className="sticky bg-background w-full z-40 top-0">
@@ -21,17 +59,71 @@ const Index = () => {
                     />
                 </Link>
                 <div className="gap-5 justify-center items-center hidden md:flex">
-                    <Link to="/blogs" className="text-base hover:text-secondary hover:scale-105"
-                    >Blogs</Link>
+                    {
+                        avatar &&
+
+                        <>
+                            <Link to="/chatroom/" className='hover:scale-105 hover:rotate-6 duration-100'>
+                                <i className="uil uil-chat text-2xl"></i>
+                            </Link>
+
+                            <Link className="relative group" id="notification__pulse">
+                                <div className="group-hover:scale-105 group-hover:rotate-6 duration-100">
+                                    <i className="uil uil-bell text-3xl"></i>
+                                </div>
+                                <div className="absolute top-0 -right-1" id="notification__pulse">
+                                    <span className="relative flex h-3 w-3">
+                                        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
+                                        <span className="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span>
+                                    </span>
+                                </div>
+                            </Link>
+
+
+                            {
+                                isSeeker ?
+                                    (
+                                        <a href="/applications" className="text-base hover:text-secondary hover:scale-105">My Applications</a>
+                                    ) :
+                                    (
+                                        <a href="/mypets" className="text-base hover:text-secondary hover:scale-105">My Pets</a>
+                                    )
+                            }
+                        </>
+                    }
+
+
+
+                    <Link to="/blogs" className="text-base hover:text-secondary hover:scale-105">Blogs</Link>
                     <div className="bg-text w-[0.7px] h-3/4"></div>
-                    <Link
-                        to="/login"
-                        className="py-3 px-6 border-text border rounded-3xl hover:bg-secondary hover:scale-105 duration-150"
-                    >Login</Link>
-                    <Link
-                        to="/signup"
-                        className="py-3 px-6 border-text border rounded-3xl bg-accent-100 hover:scale-105 text-background hover:bg-secondary hover:text-text duration-150"
-                    >Sign up</Link>
+                    {
+                        avatar ? (<>
+                            <div className="relative">
+                                <button id="profile-menu-toggle" onClick={() => {
+                                    setOpenProfileMenu(prev => !prev);
+                                }}>
+                                    <img src={avatar}
+                                        alt='avatar'
+                                        className="rounded-full hover:scale-105 active:scale-95 duration-200 w-[35px] aspect-square object-cover" />
+                                </button>
+                                <div id="profile-dropdown-menu" className={`absolute flex-col mt-2 w-[200px] right-0 ${openProfileMenu ? 'flex' : 'hidden'}`}>
+                                    <Link to="/manageaccount"
+                                        className="bg-white px-5 py-3 hover:bg-[#949494] rounded-t-lg">Manage Account</Link>
+                                    <button className="bg-white px-5 py-3 hover:bg-[#949494] rounded-b-lg" onClick={handleLogout}>Sign Out</button>
+                                </div>
+                            </div>
+                        </>) : (<>
+                            <Link
+                                to="/login"
+                                className="py-3 px-6 border-text border rounded-3xl hover:bg-secondary hover:scale-105 duration-150"
+                            >Login</Link>
+                            <Link
+                                to="/signup"
+                                className="py-3 px-6 border-text border rounded-3xl bg-accent-100 hover:scale-105 text-background hover:bg-secondary hover:text-text duration-150"
+                            >Sign up</Link>
+                        </>)
+                    }
+
                 </div>
                 <button className="md:hidden" id="menu-bar" onClick={() => setOpenNav(prev => !prev)}>
                     <img
@@ -51,20 +143,51 @@ const Index = () => {
             <div
                 className={"w-full bg-secondary flex-col md:hidden justify-between z-40 absolute h-screen " + (openNav ? 'block' : 'hidden')}
                 id="mobile_menu"
+                onClick={() => {setOpenNav(false)}}
             >
                 <div className="w-full flex flex-col">
-                    <Link
-                        className="w-full bg-primary p-8 text-xl text-background hover:bg-accent-200"
-                        to="/signup"
-                    >Sign Up</Link>
-                    <Link
-                        className="w-full bg-primary p-8 text-xl text-background hover:bg-accent-200"
-                        to="/login"
-                    >Log In</Link>
                     <Link
                         to="/blogs"
                         className="w-full bg-primary p-8 text-xl text-background hover:bg-accent-200"
                     >Blogs</Link>
+                    {avatar ? (<>
+                        <Link
+                            className="w-full bg-primary p-8 text-xl text-background hover:bg-accent-200"
+                            to="/chats"
+                        >Chats</Link>
+                        <Link
+                            className="w-full bg-primary p-8 text-xl text-background hover:bg-accent-200"
+                            to="/notifications"
+                        >Notifications</Link>
+
+                        {isSeeker ? (
+                            <Link
+                                className="w-full bg-primary p-8 text-xl text-background hover:bg-accent-200"
+                                to="/myapplications"
+                            >My Applications</Link>) : (
+                            <Link
+                                className="w-full bg-primary p-8 text-xl text-background hover:bg-accent-200"
+                                to="/mypets"
+                            >My Pets</Link>)}
+
+                        <Link
+                            className="w-full bg-[#c57f2f] p-8 text-xl text-background hover:bg-accent-200"
+                            to="/manageAccount"
+                        >Manage Account</Link>
+                        <button
+                            className="w-full bg-[#b22626] p-8 text-xl text-background hover:bg-accent-200 text-left"
+                            onClick={handleLogout}
+                        >Sign Out</button>
+                    </>) : (<>
+                        <Link
+                            className="w-full bg-primary p-8 text-xl text-background hover:bg-accent-200"
+                            to="/signup"
+                        >Sign Up</Link>
+                        <Link
+                            className="w-full bg-primary p-8 text-xl text-background hover:bg-accent-200"
+                            to="/login"
+                        >Log In</Link>
+                    </>)}
                 </div>
             </div>
         </header>
