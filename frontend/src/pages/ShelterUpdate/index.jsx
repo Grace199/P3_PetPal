c6fdd8d44ce78fd9ebbaacd1d3357a75457a8c96
Nishi <humayuranishi@gmail.com>
2023-12-09 06:08:34 -0500

    update mostly done

--- frontend/src/pages/ShelterUpdate/index.jsx
+++ frontend/src/pages/ShelterUpdate/index.jsx
@@ -7,11 +7,14 @@ const ShelterUpdate = () => {
     const [id, setId] = useState(0);
     const [isSeeker, setIsSeeker] = useState(null);
     const [shelter, setShelter] = useState(null);
+    const [name, setName] = useState("");
     const [description, setDescription] = useState("");
     const [address, setAddress] = useState("");
     const [city, setCity] = useState("");
     const [province, setProvince] = useState("");
     const [phoneNumber, setPhoneNumber] = useState(0);
+    const [profilePic, setProfilePic] = useState(null);
+    const [error, setError] = useState("");
 
     const navigate = useNavigate();
 
@@ -27,13 +30,13 @@ const ShelterUpdate = () => {
                     setIsSeeker(JSON.parse(localStorage.getItem("isSeeker")));
                     setId(parseInt(localStorage.getItem("userID")));
 
+                    setName(data.account.name);
                     setDescription(data.description);
                     setAddress(data.address);
                     setCity(data.city);
                     setProvince(data.province);
                     setPhoneNumber(data.phone_number);
-
-
+                    setProfilePic(data.account.avatar);
                 } else {
                     console.error("Error during fetch: ", res);
                 }
@@ -44,9 +47,10 @@ const ShelterUpdate = () => {
         fetchData();
     }, [shelterID, navigate]);
 
-    const handleAddressChange = (event) => {
-        const newAddress = event.target.value;
-        setAddress(newAddress);
+
+    const handleNameChange = (event) => {
+        const newName = event.target.value;
+        setName(newName);
     }
 
     const handleDescriptionChange = (event) => {
@@ -59,15 +63,60 @@ const ShelterUpdate = () => {
         setCity(newCity);
     }
 
+    const handleAddressChange = (event) => {
+        const newAddress = event.target.value;
+        setAddress(newAddress);
+    }
+
     const handleProvinceChange = (event) => {
         const newProvince = event.target.value;
         setProvince(newProvince);
     }
 
+    const handlePhoneNumberChange = (event) => {
+        const newPhoneNumber = event.target.value;
+        setPhoneNumber(newPhoneNumber);
+    }
+
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        setProfilePic(file);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const shelterData = {
+                name: name,
+                description: description,
+                address: address,
+                city: city,
+                province: province,
+                phone_number: phoneNumber,
+                account__avatar: profilePic
+            }
+            const res = await ajax_or_login(`/accounts/shelter/${shelterID}/`, { method: "PATCH", body: JSON.stringify(shelterData), headers: { 'Content-Type': 'application/json' } }, navigate);
+            if (!res.ok) {
+                const data = await res.json();
+                if ('detail' in data) {
+                    if (data.detail === "Not found.") {
+                        setError("Invalid Application");
+                    }
+                    else {
+                        setError(data.detail);
+                    }
+                }
+                console.error("Error during fetch: ", res);
+            }
+        } catch (error) {
+            console.error("Error during fetch: ", error);
+        }
+    }
+
     return (
         <>
             <main className="px-mobile md:px-tablet xl:px-desktop pt-6 sm:pt-16 h-full">
-                <div className="bg-[#F2F5FD] w-full">
+                <div className="bg-[#F2F5FD] w-full rounded-b-2xl">
                     <div className="rounded-2xl flex flex-col overflow-hidden gap-4">
                         <div className="bg-secondary flex flex-row justify-between items-center h-12 px-4 sm:px-12">
                             <p class="text-text text-xl font-semibold">Update Account</p>
@@ -77,20 +126,38 @@ const ShelterUpdate = () => {
                         </div>
                         <div className="flex flex-col px-6 sm:px-12 md:px-20 gap-6 pb-8">
                             <div className="flex flex-col gap-2">
+                                <p className="max-sm:text-sm">Shelter Name:</p>
+                                <textarea rows="1" className="w-full p-3 border border-secondary rounded-[7px] font-light resize-none max-sm:text-sm" value={name} onChange={handleNameChange} />
+                            </div>
+                            <div className="flex flex-col gap-2">
                                 <p className="max-sm:text-sm">Mission Statement:</p>
                                 <textarea rows="5" className="w-full p-3 border border-secondary rounded-[7px] font-light resize-none max-sm:text-sm" value={description} onChange={handleDescriptionChange} />
                             </div>
                             <div className="flex flex-col gap-2">
-                                <p class="max-sm:text-sm">Address:</p>
+                                <p className="max-sm:text-sm">Address:</p>
                                 <textarea rows="1" className="w-full p-3 border border-secondary rounded-[7px] font-light resize-none max-sm:text-sm" value={address} onChange={handleAddressChange} />
                             </div>
                             <div className="flex flex-col gap-2">
-                                <p class="max-sm:text-sm">City:</p>
+                                <p className="max-sm:text-sm">City:</p>
                                 <textarea rows="1" className="w-full p-3 border border-secondary rounded-[7px] font-light resize-none max-sm:text-sm" value={city} onChange={handleCityChange} />
                             </div>
                             <div className="flex flex-col gap-2">
-                                <p class="max-sm:text-sm">Province:</p>
+                                <p className="max-sm:text-sm">Province:</p>
                                 <textarea rows="1" className="w-full p-3 border border-secondary rounded-[7px] font-light resize-none max-sm:text-sm" value={province} onChange={handleProvinceChange} />
+                            </div>
+                            <div className="flex flex-col gap-2">
+                                <p className="max-sm:text-sm">Phone Number:</p>
+                                <textarea rows="1" className="w-full p-3 border border-secondary rounded-[7px] font-light resize-none max-sm:text-sm" value={phoneNumber} onChange={handlePhoneNumberChange} />
+                            </div>
+                            <div className="flex flex-col gap-2">
+                                <p className="max-sm:text-sm">Profile Picture:</p>
+                                <input type="file" onChange={handleFileChange} />
+                            </div>
+                            <div className="flex justify-center">
+                                <p className="font-semibold text-xs sm:text-sm text-red-500">{error}</p>
+                            </div>
+                            <div className="flex justify-center md:justify-end">
+                                <button className="bg-accent-100 rounded-full px-10 py-4 text-[#F2F5FD] font-bold text-lg" onClick={handleSubmit}>Update</button>
                             </div>
                         </div>
                     </div>
