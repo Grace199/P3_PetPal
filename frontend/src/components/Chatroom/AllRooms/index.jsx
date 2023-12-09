dc80aec1a236af22ecaa99a7b3cf177c7a8328eb
Nishi <humayuranishi@gmail.com>
2023-12-09 07:46:56 -0500

    idk

--- frontend/src/components/Chatroom/AllRooms/index.jsx
+++ frontend/src/components/Chatroom/AllRooms/index.jsx
@@ -1,46 +1,73 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Room from '../Room'
+import { UserContext } from '../../../contexts/UserContext';
+import { useNavigate } from 'react-router-dom';
+import { ajax_or_login } from '../../../util/ajax';
 
 const Index = ({ setChatID }) => {
-    const [allRooms, setAllRooms] = useState([]);
+
+    const [rooms, setRooms] = useState([]);
+    const [id, setId] = useState(0);
+    const [isSeeker, setIsSeeker] = useState(null);
     const [open, setOpen] = useState(false);
+    const navigate = useNavigate();
+
+    // useEffect(() => {
+    //     async function fetchData() {
+    //         if (id !== -1) {
+    //             const isSeeker = JSON.parse(localStorage.getItem("isSeeker"));
+    //             const apiUrl = `/accounts/${isSeeker ? 'seeker' : 'shelter'}/${id}/`;
+    //             const res = await ajax_or_login(apiUrl, {
+    //                 method: "GET",
+    //             }, navigate);
+
+    //             if (res.ok) {
+    //                 const data = await res.json();
+    //                 setAvatar(data.account.avatar);
+    //                 localStorage.setItem('avatar', data.account.avatar);
+    //                 setIsSeeker(isSeeker);
+    //             }
+    //         }
+    //         setId(parseInt(localStorage.getItem("userID")));
+    //     }
+
+    //     fetchData();
+    // }, [id, navigate]);
 
     useEffect(() => {
-        setAllRooms([1]);
-    }, [])
+        const fetchData = async () => {
+            try {
+                setIsSeeker(JSON.parse(localStorage.getItem("isSeeker")));
+                setId(parseInt(localStorage.getItem("userID")));
+
+                const apiUrl = `/accounts/${isSeeker ? 'seeker' : 'shelter'}/${id}/`;
+                const res = await ajax_or_login(apiUrl, { method: "GET" }, navigate);
+
+                if (res.ok) {
+                    const data = await res.json();
+                    setRooms(data);
+                } else {
+                    console.error("Error during fetch: ", res);
+                }
+            } catch (error) {
+                console.error("Error during fetch: ", error);
+            }
+        };
+        fetchData();
+    }, [navigate]);
 
     return (
         <>
-            < div
-                className={`bg-background-secondary max-md:w-screen max-md:fixed w-full md:w-[360px] z-10 h-full flex-col border-r border-black  overflow-y-scroll flex ${open ? 'block' : 'max-md:hidden'}`}
-                onClick={() => setOpen(false)}
-            >
-                {allRooms?.length > 0 ? (<>
-                    <Room setChatID={setChatID} chatID={1} image={'unknown'} />
-                    <Room setChatID={setChatID} chatID={2} image={'unknown'} />
-                    <Room setChatID={setChatID} chatID={3} image={'unknown'} />
-                    <Room setChatID={setChatID} chatID={4} image={'unknown'} />
-                    <Room setChatID={setChatID} chatID={5} image={'unknown'} />
-                    <Room setChatID={setChatID} chatID={6} image={'unknown'} />
-                    <Room setChatID={setChatID} chatID={7} image={'unknown'} />
-                    <Room setChatID={setChatID} chatID={8} image={'unknown'} />
-                    <Room setChatID={setChatID} chatID={8} image={'unknown'} />
-                    <Room setChatID={setChatID} chatID={8} image={'unknown'} />
-                    <Room setChatID={setChatID} chatID={8} image={'unknown'} />
-                    <Room setChatID={setChatID} chatID={8} image={'unknown'} />
-                    <Room setChatID={setChatID} chatID={8} image={'unknown'} />
-                    <Room setChatID={setChatID} chatID={8} image={'unknown'} />
-                    <Room setChatID={setChatID} chatID={8} image={'unknown'} />
-                </>)
-                    :
-                    (
-                        <div className='w-full h-full flex justify-center items-center'>
-                            <p>No chats</p>
-                        </div>
-
-                    )
+            <div className={`bg-background-secondary max-md:w-screen max-md:fixed w-full md:w-[360px] z-10 h-full flex-col border-r border-black  overflow-y-scroll flex ${open ? 'block' : 'max-md:hidden'}`} onClick={() => setOpen(false)}>
+                {rooms && rooms.map(room => (
+                    <Room ></Room>
+                ))}
+                {!rooms &&
+                    <div className='w-full h-full flex justify-center items-center'>
+                        <p>No chats</p>
+                    </div>
                 }
-            </div >
+            </div>
 
             <button className={`absolute pt-1 pl-3 hover:text-primary md:hidden ${open ? 'hidden' : 'block'}`} onClick={() => { setOpen(true) }}><i className="uil uil-arrow-circle-left text-5xl"></i></button>
         </>
